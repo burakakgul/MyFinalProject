564c609a8b07c71075abbb81c6204037debd14a1
asus <asus@ASUSLAPTOPHBA>
2021-02-09 23:15:56 +0300

    8. Gün Ders Sonu

---
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{ //Context: Db tabloları ile proje classlarını bağlamak
+    public class NorthwindContext : DbContext
+    {
+        //Bu method ile hangi veritabanı ile ilişkilendireceğimizi belirteceğiz
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_Connection=true");
+        }
+        //DbSet ile hangi Class hangi Tabloya denk geldiğini belirtmek için
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
